5705be2507c8502443fd1bcd21fb281ab76e4473
Jxnatan7 <jgabrieldesouzasantos@gmail.com>
2023-04-26 09:55:03 -0300

    Criação da variável da porta para deploy

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ app.register(cors)
 app.register(appRoutes)
 
 app.listen({
-    port: 3333,
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
     host: "0.0.0.0"
-}).then(() => console.log(" HTTPServer running"))
+}).then(() => console.log(" HTTP Server running"))
     .catch((err) => console.log(`Erro; ${err}`))
